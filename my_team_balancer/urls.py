ae76e891230b2cf0d8afa0afeecbd0b2b888c13f
Mark Williams <mark-mw-williams@hotmail.com>
2026-05-11 22:14:37 +0100

    Fix Static Root and URLS

--- my_team_balancer/urls.py
+++ my_team_balancer/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('login')),
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
     path('match/', include('match.urls')),
