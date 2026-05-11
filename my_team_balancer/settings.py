3b5488438e4e957508ed0c8a2223218ef2f3b0f0
Mark Williams <mark-mw-williams@hotmail.com>
2026-05-11 22:22:11 +0100

    fix static path

--- my_team_balancer/settings.py
+++ my_team_balancer/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/markmw642/my_team_balancer/static'
+STATIC_ROOT = '/home/MarkMW642/my_team_balancer/static'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 
